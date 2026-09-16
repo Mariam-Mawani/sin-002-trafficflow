ac9ca63335fd30c0a8754fc846d2e4920f044621
mariam mawani <mariammawani@gmail.com>
2026-09-16 11:12:14 +0200

    Added test class accepts levels tests

--- congestion-service/src/test/java/co/wethinkcode/trafficflow/CongestionServiceAppTest.java
+++ congestion-service/src/test/java/co/wethinkcode/trafficflow/CongestionServiceAppTest.java
@@ -2,5 +2,12 @@ package co.wethinkcode.trafficflow;
 
 public class CongestionServiceAppTest {
 
+    @Test
+    void acceptsLevelsWithinRange() {
+        assertTrue(isValidLevel(0));
+        assertTrue(isValidLevel(4));
+        assertTrue(isValidLevel(8));
+    }
+
 
 }
