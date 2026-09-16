030362db3f9d05627ae9c579ee413169b46b02a8
mariam mawani <mariammawani@gmail.com>
2026-09-16 15:30:32 +0200

    added congestion test

--- routing-service/src/test/java/co/wethinkcode/trafficflow/RoutingServiceAppTest.java
+++ routing-service/src/test/java/co/wethinkcode/trafficflow/RoutingServiceAppTest.java
@@ -1,4 +1,14 @@
 package co.wethinkcode.trafficflow;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoutingServiceAppTest {
+
+    @Test
+    void noCongestionGivesJustTheBaseTime() {
+        assertEquals(5.0, estimateMinutes(0));
+    }
+
 }
