f82911d72f4e0bf475a245af0d633d0377cc99b3
mariam mawani <mariammawani@gmail.com>
2026-09-17 11:21:27 +0200

    created and added CongestionTopicPublisher test

---
+++ congestion-service/src/test/java/co/wethinkcode/trafficflow/CongestionTopicPublisherTest.java
@@ -0,0 +1,14 @@
+package co.wethinkcode.trafficflow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CongestionTopicPublisherTest {
+
+    @Test
+    void payloadContainsTheLevel() {
+        assertEquals("{\"level\":0}", CongestionTopicPublisher.buildPayload(0));
+        assertEquals("{\"level\":8}", CongestionTopicPublisher.buildPayload(8));
+    }
+}
